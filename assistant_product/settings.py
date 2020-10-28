19eb90003505b19ceba499a9576430a1b9e3a068
sag163 <60653379+sag163@users.noreply.github.com>
2020-10-28 21:19:48 +0400

    edit setting2

--- assistant_product/settings.py
+++ assistant_product/settings.py
@@ -143,13 +143,11 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
